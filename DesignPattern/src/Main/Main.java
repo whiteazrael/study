60a995a164f84205f13c240cd5aff136242fe061
HyungCheol Yoon <hyungcheol.yoon@nhnent.com>
2015-05-04 14:16:22 +0900

    delegate 코드 추가

--- DesignPattern/src/Main/Main.java
+++ DesignPattern/src/Main/Main.java
@@ -2,13 +2,19 @@ package Main;
 
 import BuilderPattern.BuilderPattern;
 import DelegatePattern.Avengers;
+import DelegatePattern.Combatant;
 import DelegatePattern.Shield;
 
 public class Main {
 	public static void main(String[] args) {
 		BuilderPattern builderPattern = new BuilderPattern.Builder().a(1).b(2).c(3).d(4).build();
 		
-		Avengers shield = new Shield();
+		Shield shield = new Shield();
+		shield.executeMission();
+		shield.rest();
+		
+		Avengers captainAmerica = new Combatant("captainAmerica");
+		shield.delegateTo(captainAmerica);
 		shield.executeMission();
 		shield.rest();
 	}
